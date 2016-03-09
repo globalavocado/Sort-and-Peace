b7abc54e43bc9297e7bbc9e7d32fd55b8c311a05
globalavocado <globalavocado@gmail.com>
2016-03-09 13:38:32 +0000

    added back vars because drop area restrictions had stopped working

--- js/tolstoy.js
+++ js/tolstoy.js
@@ -1,20 +1,39 @@
-Sortable.create(pierre, {group: 'the_pierres'});
+var the_pierres =  document.getElementById("pierre");
+var the_natashas =  document.getElementById("natasha");
+var the_andreis =  document.getElementById("andrei");
 
-Sortable.create(natasha, {group: 'the_natashas'});
+Sortable.create(
+	pierre, {
+		group: 'the_pierres'
+	});
 
-Sortable.create(andrei, {group: 'the_andreis'});
+Sortable.create(
+	natasha, {
+		group: 'the_natashas'
+	});
+
+Sortable.create(
+	andrei, {
+		group: 'the_andreis'
+	});
 
 Sortable.create(
 	droppierre, {
-		group: {put: ['the_pierres']}
+		group: {
+			put: ['the_pierres']
+		}
 	});
 
 Sortable.create(
 	dropnatasha, {
-		group: {put: ['the_natashas']}
+		group: {
+			put: ['the_natashas']
+		}
 	});
 
 Sortable.create(
 	dropandrei, {
-		group: {put: ['the_andreis']}
+		group: {
+			put: ['the_andreis']
+		}
 	});
