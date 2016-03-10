1109ac7c379ad08e5e5bfa9865da161574f75b13
globalavocado <globalavocado@gmail.com>
2016-03-10 14:15:11 +0000

    characters can be moved between their photo & drop areas

--- js/tolstoy.js
+++ js/tolstoy.js
@@ -2,24 +2,31 @@ var the_pierres =  document.getElementById("pierre");
 var the_natashas =  document.getElementById("natasha");
 var the_andreis =  document.getElementById("andrei");
 
-Sortable.create(
-	pierre, {
-		group: 'the_pierres'
+Sortable.create(pierre, {
+		group: {
+			name:'the_pierres',
+			put: ['droppierre']
+		}
 	});
 
-Sortable.create(
-	natasha, {
-		group: 'the_natashas'
+Sortable.create(natasha, {
+		group: {
+			name:'the_natashas',
+			put: ['dropnatasha']
+		}
 	});
 
-Sortable.create(
-	andrei, {
-		group: 'the_andreis'
+Sortable.create(andrei, {
+		group: {
+			name:'the_andreis',
+			put: ['dropandrei']
+		}
 	});
 
 Sortable.create(
 	droppierre, {
 		group: {
+			name: 'droppierre',
 			put: ['the_pierres']
 		}
 	});
@@ -27,6 +34,7 @@ Sortable.create(
 Sortable.create(
 	dropnatasha, {
 		group: {
+			name: 'dropnatasha',
 			put: ['the_natashas']
 		}
 	});
@@ -34,6 +42,7 @@ Sortable.create(
 Sortable.create(
 	dropandrei, {
 		group: {
+			name: 'dropandrei',
 			put: ['the_andreis']
 		}
 	});
